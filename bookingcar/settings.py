629f443aecc3c452fdf03435108b00d3d40fb9c5
Ngo Don <anhdon139@gmail.com>
2022-03-25 21:06:16 +0700

    apply

--- bookingcar/settings.py
+++ bookingcar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-5b)nsnn49&vs0*32d#zi*y43bo=6!9prpwqtmyxmmt!-wkd=i^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
